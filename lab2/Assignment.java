75c9af5f50dcf23bc752edc651688122e73cd061
Sridevi523 <sridevimahapatro6@gmail.com>
2024-05-27 09:16:21 +0530

    Add files via upload

---
+++ lab2/Assignment.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Assignment {
+	 public static void main(String[] args)
+	    {
+	        
+	       int num = 10;
+	       int num2=20;
+	       int sum=num+num2;
+	        String name = "Sri";
+	 
+	        System.out.println("num is : " + sum);
+	        System.out.println("name is : " + name);
+	    }
+
+}
