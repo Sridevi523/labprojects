3b30853e531f14d5f213f00e5e1e20b38ef5d510
Sridevi523 <sridevimahapatro6@gmail.com>
2024-05-27 09:17:51 +0530

    Add files via upload

---
+++ lab1/Primitive.java
@@ -0,0 +1,33 @@
+import java.util.*;
+ class Primitive 
+ {
+	 public static void main(String args[]) {
+	      boolean var1 = true;
+	      char var2 = 'S';
+	      char var3 = 'R';
+	      int a = 14;
+	      short s = 2;
+	      byte b = 6;
+	      long l = 12936213373l;
+	      float f = 65.8892f;
+	      double d = 576.978d;
+	   
+            System.out.println(var2);
+	        System.out.println(var3);
+	        System.out.println("The integer variable is " + a + '\n');
+	        System.out.println("The short variable is " + s  + '\n');
+	        System.out.println("The byte variable is " + b + '\n');
+	        System.out.println("The long variable is " + l);
+	        System.out.println("The float variable is " + f);
+	        System.out.println("The double variable is " + d);
+	     
+	      if (var1 == true) 
+	      {
+	          System.out.println("Boolean value is True");
+	      }
+	     
+	      
+	    
+	
+	 }
+}
