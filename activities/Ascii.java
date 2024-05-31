080e689200285ae352fade3c0277993d223bc223
Sridevi523 <sridevimahapatro6@gmail.com>
2024-05-31 22:26:11 +0530

    Add files via upload

---
+++ activities/Ascii.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+	public class Ascii {
+	    public static void main(String[] args) {
+	    	
+	 
+	    	        Scanner sc=new Scanner(System.in);
+	    	        System.out.println("Enter the character ASCII value");
+	    	        char ch=sc.next().charAt(0);
+	    	        int value=ch;
+	    	        System.out.println("Character is "+ch+" and ASCII value is "+value);
+	    	     }
+	    	
+
+	    }
